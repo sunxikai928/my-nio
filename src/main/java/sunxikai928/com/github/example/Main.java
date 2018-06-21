ad3e45d55f1dc55b9ab39124a554571fd1b690f9
sunxikai <xikai.sun@pactera.com>
2018-06-21 22:08:29 +0800

    增加日志打印

--- src/main/java/sunxikai928/com/github/example/Main.java
+++ src/main/java/sunxikai928/com/github/example/Main.java
@@ -1,10 +1,15 @@
 package sunxikai928.com.github.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by sunxikai on 18/6/21.
  */
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("服务器启动了");
+        logger.info("服务器启动了");
     }
 }
