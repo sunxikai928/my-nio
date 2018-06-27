58f44d2f72a91e164329b426f8143458bc6b929a
sunxikai <xikai.sun@pactera.com>
2018-06-27 20:09:45 +0800

    函数式编程注解

--- src/main/java/sunxikai928/com/github/IMessageProcessor.java
+++ src/main/java/sunxikai928/com/github/IMessageProcessor.java
@@ -1,8 +1,10 @@
 package sunxikai928.com.github;
 
 /**
+ * {@link FunctionalInterface} 函数式编程接口中只有一个方法,后续不可添加,可用于函数式编程
  * Created by jjenkov on 16-10-2015.
  */
+@FunctionalInterface
 public interface IMessageProcessor {
 
     void process(Socket socket);
